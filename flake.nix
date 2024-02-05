efc11f90fb08f184c6d9c67d90beb31d6471bb9b
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-02-05 19:55:08 +0000

    Add development flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+    description = "Flake for Holochain client development";
+
+    inputs = {
+        versions.url = "github:holochain/holochain?dir=versions/0_2";
+
+        versions.inputs.holochain.url = "github:holochain/holochain/holochain-0.2.5";
+
+        holochain = {
+            url = "github:holochain/holochain";
+            inputs.versions.follows = "versions";
+        };
+
+        nixpkgs.follows = "holochain/nixpkgs";
+    };
+
+    outputs = inputs @ { ... }:
+    inputs.holochain.inputs.flake-parts.lib.mkFlake { inherit inputs; }
+    {
+        systems = builtins.attrNames inputs.holochain.devShells;
+        perSystem = { config, pkgs, system, ... }: {
+            devShells.default = pkgs.mkShell {
+                inputsFrom = [
+                    inputs.holochain.devShells.${system}.holonix
+                ];
+                packages = [
+                    (pkgs.python3.withPackages (python-pkgs: [
+                        python-pkgs.poetry-core
+                    ]))
+                ];
+            };
+        };
+    };
+}
